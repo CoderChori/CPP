877819852521c6943f9336131e44fbbd27aeb944
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-12-23 09:42:04 +0530

    Create p17.cpp

---
+++ p17.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int row = 1;
+   char ch = 'A';
+  while (row <= n) {
+    int col = 1;
+    while (col <= row) { 
+      cout << ch;
+      col = col + 1;
+     
+    }
+    cout << endl;
+     ch = ch + 1;
+    row = row + 1;
+  }
+}
+
+/*
+I/p: 3
+A
+BB
+CCC
+*/
